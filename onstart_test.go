4130439af8fd384f31d5fa1d5fe33d1897c5a622
Grayson Koonce <grayson@uber.com>
2018-02-16 08:53:36 -0800

    cleanup test

--- onstart_test.go
+++ onstart_test.go
@@ -8,54 +8,49 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestOnStart(t *testing.T) {
+func TestOnStartOnStop(t *testing.T) {
+	called := 0
 	app := New(
 		Provide(newA, newB),
 		OnStart(
-			func(a a, b b) {
+			func(a *a, b *b) {
 				assert.True(t, a.Started)
 				assert.True(t, b.Started)
+				called++
 			},
-			func(b b, a a) {
+			func(b *b, a *a) {
 				assert.True(t, a.Started)
 				assert.True(t, b.Started)
+				called++
 			},
 		),
-	)
-	require.NoError(t, app.Err())
-	err := app.Start(context.Background())
-	require.NoError(t, err)
-}
-
-func TestOnStop(t *testing.T) {
-	app := New(
-		Provide(newA, newB),
 		OnStop(
-			func(a a, b b) {
+			func(a *a, b *b) {
 				assert.True(t, a.Started)
 				assert.True(t, b.Started)
+				called++
 			},
-			func(b b, a a) {
+			func(b *b, a *a) {
 				assert.True(t, a.Started)
 				assert.True(t, b.Started)
+				called++
 			},
 		),
 	)
+
 	require.NoError(t, app.Err())
+	require.NoError(t, app.Start(context.Background()))
+	require.NoError(t, app.Stop(context.Background()))
 
-	err := app.Start(context.Background())
-	require.NoError(t, err)
-
-	err = app.Stop(context.Background())
-	require.NoError(t, err)
+	assert.Equal(t, 4, called)
 }
 
 type a struct {
 	Started bool
 }
 
-func newA(lifecycle Lifecycle) a {
-	a := a{}
+func newA(lifecycle Lifecycle) *a {
+	a := &a{}
 	lifecycle.Append(Hook{
 		OnStart: func(ctx context.Context) error {
 			a.Started = true
@@ -69,8 +64,8 @@ type b struct {
 	Started bool
 }
 
-func newB(lifecycle Lifecycle) b {
-	b := b{}
+func newB(lifecycle Lifecycle) *b {
+	b := &b{}
 	lifecycle.Append(Hook{
 		OnStart: func(ctx context.Context) error {
 			b.Started = true
